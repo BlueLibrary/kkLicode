97590452d287a39368c02504064ac924b5382175
icsaint <78400679@qq.com>
2017-09-26 17:38:27 +0800

    修正

--- servererizo.sh
+++ servererizo.sh
@@ -14,6 +14,7 @@ LIB_DIR=$BUILD_DIR/libdeps
 PREFIX_DIR=$LIB_DIR/build/
 FAST_MAKE=''
 export ERIZO_HOME=$ROOT/erizo
-#echo $ERIZO_HOME
+echo $ERIZO_HOME
+echo $BUILD_DIR/libdeps/build/include
 cd servererizo
 make

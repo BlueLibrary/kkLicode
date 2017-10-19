6d171896973a6ad870837960b6a10a626212a894
icsaint <78400679@qq.com>
2017-10-19 08:47:02 +0800

    。。。

--- servererizo.sh
+++ servererizo.sh
@@ -15,7 +15,7 @@ export ERIZO_HOME=$ROOT/erizo
 export SERVER_HOME=$ROOT/servererizo
 echo $ERIZO_HOME
 echo $BUILD_DIR/libdeps/build/include
-./$ERIZO_HOME/generateProject.sh
-./$ERIZO_HOME/buildProject.sh
+.$ERIZO_HOME/generateProject.sh
+.$ERIZO_HOME/buildProject.sh
 cd servererizo
 make
